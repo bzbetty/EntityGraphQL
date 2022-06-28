d6d93f759367c373371d22cefa9a61470e63a35e
Betty <sam.mckoy@enlighten.co.nz>
2022-06-28 21:52:59 +1200

    sample of broken behaviour

--- src/tests/EntityGraphQL.Tests/QueryTests/QueryTests.cs
+++ src/tests/EntityGraphQL.Tests/QueryTests/QueryTests.cs
@@ -369,5 +369,41 @@ query {
             Assert.Equal("Dog", animals[0].__typename);
             Assert.Equal("Cat", animals[1].__typename);
         }
+
+
+        public class DeepContext
+        {
+            public IList<LevelOne> LevelOnes { get; set; }
+
+            public class LevelOne
+            {
+                public LevelTwo LevelTwo { get; set; }
+            }
+
+            public class LevelTwo
+            {
+                public ICollection<LevelThree> Level3 { get; set; }
+            }
+
+            public class LevelThree
+            {
+                public string Name { get; set; }
+            }
+        }
+
+        [Fact]
+        public void TestDeepQuery()
+        {
+            var schemaProvider = SchemaBuilder.FromObject<DeepContext>(false);
+            var gql = new QueryRequest
+            {
+                Query = @"query deep { levelOnes { levelTwo { level3 { name }} }}",
+            };
+
+            var testSchema = new DeepContext();
+
+            var results = schemaProvider.ExecuteRequest(gql, testSchema, null, null);
+            Assert.Null(results.Errors);
+        }
     }
 }
